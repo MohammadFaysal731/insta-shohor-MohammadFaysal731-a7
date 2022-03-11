b07bf689cca54cfd2ccb6b6028bca8e2b7756a77
MohammadFaysal731 <www.faysal731@gmail.com>
2022-03-11 07:47:12 +0600

    Find  User profile pictur

--- js/app.js
+++ js/app.js
@@ -51,7 +51,6 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-  const image = post.image;
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -62,7 +61,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -76,7 +75,7 @@ const createPost = (post) => {
                 <div class="post__medias">
                   <img
                     class="post__media"
-                    src="${image}"
+                    src="${post.image}"
                     alt="Post Content"
                   />
                 </div>
