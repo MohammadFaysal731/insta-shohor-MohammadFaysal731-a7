5ef4184f78e63fa558aad8cb0fa267cbf69f406d
MohammadFaysal731 <www.faysal731@gmail.com>
2022-03-11 16:28:58 +0600

    Add new section and ans 3 quetions

--- js/app.js
+++ js/app.js
@@ -35,13 +35,15 @@ const switchTab = (id) => {
     document.getElementById("posts").style.display = "grid";
     document.getElementById("liked").style.display = "none";
     document.getElementById("reported").style.display = "none";
-  } else if (id === "liked") {
+  }
+  else if (id === "liked") {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
 
     displayLikedPosts();
-  } else {
+  }
+  else {
     document.getElementById("reported").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("liked").style.display = "none";
