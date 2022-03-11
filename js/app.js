fd82cec892673b21c8ba1201790356beefd22c51
MohammadFaysal731 <www.faysal731@gmail.com>
2022-03-11 09:10:28 +0600

    Fix (show repot) and show which repot is repoted

--- js/app.js
+++ js/app.js
@@ -51,7 +51,6 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-  console.log(post)
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -151,7 +150,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
